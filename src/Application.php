0200a3911fb3a743b173136ac5eec287f1896029
kyoungkyoung <wkjang4@gmail.com>
2021-10-25 20:16:18 +0900

    composer.json version 수정

---
+++ src/Application.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Phpframework;
+
+use PhpFramework\Support\ServiceProvider;
+
+class Application
+{
+    private $providers = [];
+
+    public function __construct($providers = [])
+    {
+        $this-> providers = $providers;
+        array_walk($this->providers, fn($provider) => $provider::register());      
+    }
+
+    public function boot()
+    {
+        array_walk($this->providers, fn($provider) => $provider::boot());
+    }
+
+
+}
---
+++ src/Support/ServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PhpFramework\Support;
+
+abstract class ServiceProvider{
+    public static function register(){
+
+    }
+    public static function boot(){
+        
+    }
+}
